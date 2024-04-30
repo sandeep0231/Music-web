7a8648a4513d63f947c722d53ab2986348966add
Sandeep Negi <snegi@joriehp.com>
2024-04-30 19:57:33 +0530

    commit for scroll  dt

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -23,7 +23,7 @@ function FeaturedCourses() {
         <div>
             <div className="text-center">
                 <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED COURSES</h2>
-                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best</p>
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best Courses</p>
             </div>
         </div>
         <div className="mt-10 mx-8">
