02d8488ef0cdd38ae007fa9c857eb3dd453812c2
Sandeep Negi <snegi@joriehp.com>
2024-05-02 14:38:59 +0530

    commit for  wevy and tooltip animated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -16,6 +18,8 @@ export default function Home() {
   <WhyChooseUs/>
   <MusicSchoolTestimonials/>
   <UpcomingWebinars/>
+  <Instructors/>
+  <Footer/>
   </main>
   </>
   );
